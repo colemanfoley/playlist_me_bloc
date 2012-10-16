a39cd4f8b02d5e600a1606c20426f4a42fe27b48
Coleman Foley <colemanfoley@gmail.com>
2012-10-16 16:02:27 -0700

    Initial commit
    After initializing the rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlaylistMe::Application.config.secret_token = '97fb7a0039f9f8cab40ce592ceece09ed5a3400235b5c79113d2d97d485f2a354ede97f9027e5feb7b60c3e62454f85d4d96d14fca28836eb643a91bb8d847c5'
