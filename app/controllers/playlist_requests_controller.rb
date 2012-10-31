7b7f55ddf70f4ba6a62a8c3516e8ece41d2a96ba
Coleman Foley <colemanfoley@gmail.com>
2012-10-30 20:23:11 -0700

    Recreated views for PlaylistRequests controller
    Gave them same functionality as they had before I destroyed and
    regenerated the PlaylistRequests controller. Now, I no longer get any
    errors when making or viewing users.

--- app/controllers/playlist_requests_controller.rb
+++ app/controllers/playlist_requests_controller.rb
@@ -1,13 +1,26 @@
 class PlaylistRequestsController < ApplicationController
   def index
+    @playlistrequests = PlaylistRequest.all
   end
 
   def new
+    @playlistrequest = PlaylistRequest.new
   end
 
   def show
+    @playlistrequest = PlaylistRequest.find(params[:id])
   end
 
   def edit
+  end
+
+  def create
+    @playlistrequest = PlaylistRequest.new(params[:playlistrequest])
+    if @playlistrequest.save
+      redirect_to @playlistrequest
+    else
+      render :edit
+    end
+
   end
 end
